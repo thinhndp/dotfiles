3a9ef315fc1a0fe2a2f30e1914a6e9d8254d5255
thinhndp <thinh.dp.nguyen@employmenthero.com>
2024-02-07 17:00:05 +0700

    nvim integrate copilot chat

--- nvim/init.lua
+++ nvim/init.lua
@@ -198,6 +198,30 @@ require('lazy').setup({
     },
   },
 
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      debug = true,
+      show_help = "yes",
+      prompts = {
+        Explain = "Explain how it works.",
+        Review = "Review the selected code and provide concise suggestions.",
+        Tests = "Briefly explain how the selected code works, then generate unit tests.",
+        Refactor = "Refactor the code to improve clarity and readability.",
+      },
+    },
+    build = function()
+      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+    end,
+    event = "VeryLazy",
+    keys = {
+      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+      { "<leader>ccr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
+      { "<leader>ccR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
+    }
+  },
+
   -- {
   --   -- Theme inspired by Atom
   --   'navarasu/onedark.nvim',
